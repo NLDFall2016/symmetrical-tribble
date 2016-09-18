4a01e245aefdf9aab1c1699f2849423a81b60ffd
R Steiner <r.steiner@uconn.edu>
2016-09-18 19:54:47 -0400

    Add euler + improved euler to all, prep for submission

--- Homework1/hw1.m
+++ Homework1/hw1.m
@@ -39,8 +39,17 @@ x0d = 0;
 plot(t, x, '-o')
 
 title(legend(num2str(x0), num2str(x0b), num2str(x0c), num2str(x0d)), 'x0')
-
-% Euler Method:
+%
+% *Euler Method:*
+%
+% x0s = [x0 x0b x0c x0d];
+% ode1 = @(x) -3*x;
+% dxdt_eul = zeros(1,length(x0s));
+% 
+% for n = 1:length(x0s)
+%     dxdt_eul(n) = eul(ode1, 0, 5, 0.1, x0s(n));
+%     plot(0:0.1:5, dxdt_eul(n), '--')
+% end
 
 ode1 = @(x) -3*x;
 dxdt_a = eul(ode1, 0, 5, 0.1, x0);
@@ -48,10 +57,10 @@ dxdt_b = eul(ode1, 0, 5, 0.1, x0b);
 dxdt_c = eul(ode1, 0, 5, 0.1, x0c);
 dxdt_d = eul(ode1, 0, 5, 0.1, x0d);
 
-plot(0:0.1:5, dxdt_a, '--')
-plot(0:0.1:5, dxdt_b, '--')
-plot(0:0.1:5, dxdt_c, '--')
-plot(0:0.1:5, dxdt_d, '--')
+plot(0:0.1:5, dxdt_a, '--d')
+plot(0:0.1:5, dxdt_b, '--d')
+plot(0:0.1:5, dxdt_c, '--d')
+plot(0:0.1:5, dxdt_d, '--d')
 
 % Improved euler method:
 
@@ -60,10 +69,10 @@ dxdt_b2 = eul2(ode1, 0, 5, 0.1, x0b);
 dxdt_c2 = eul2(ode1, 0, 5, 0.1, x0c);
 dxdt_d2 = eul2(ode1, 0, 5, 0.1, x0d);
 
-plot(0:0.1:5, dxdt_a2, ':')
-plot(0:0.1:5, dxdt_b2, ':')
-plot(0:0.1:5, dxdt_c2, ':')
-plot(0:0.1:5, dxdt_d2, ':')
+plot(0:0.1:5, dxdt_a2, ':*')
+plot(0:0.1:5, dxdt_b2, ':*')
+plot(0:0.1:5, dxdt_c2, ':*')
+plot(0:0.1:5, dxdt_d2, ':*')
 
 hold off
 
@@ -87,7 +96,7 @@ hold off
 tspan = [0 0.36];
 x0 = 0;
 [t,x] = ode23(@(t,x) 4*x^2-16, tspan, x0); %%%%%% you can give it a vector of initial conditions!!
-plot(t, x, '-o')
+plot(t, x, '-s')
 title('xdot = 4*x^2-16')
 xlabel('t')
 ylabel('x')
@@ -96,21 +105,53 @@ hold on
 
 x0b = 2;
 [t,x] = ode23(@(t,x) 4*x^2-16, tspan, x0b);
-plot(t, x, ':d')
+plot(t, x, '-s')
 
 x0c = -3;
 [t,x] = ode23(@(t,x) 4*x^2-16, tspan, x0c);
-plot(t, x, 's')
+plot(t, x, '-s')
 
 x0d = 2.01;
 [t,x] = ode23(@(t,x) 4*x^2-16, tspan, x0d);
-plot(t, x, '--s')
+plot(t, x, '-s')
 
 x0e = 1;
 [t,x] = ode23(@(t,x) 4*x^2-16, tspan, x0e);
-plot(t, x, '-*')
+plot(t, x, '-s')
 
 title(legend(num2str(x0), num2str(x0b), num2str(x0c), num2str(x0d), num2str(x0e)), 'x0')
+
+% Euler method:
+
+ode2 = @(x) 4*x^2-16;
+tmin = 0;
+tmax = 0.36;
+tstep = 0.01;
+dxdt_a = eul(ode2, tmin, tmax, tstep, x0);
+dxdt_b = eul(ode2, tmin, tmax, tstep, x0b);
+dxdt_c = eul(ode2, tmin, tmax, tstep, x0c);
+dxdt_d = eul(ode2, tmin, tmax, tstep, x0d);
+dxdt_e = eul(ode2, tmin, tmax, tstep, x0e);
+
+plot(tmin:tstep:tmax, dxdt_a, '--d')
+plot(tmin:tstep:tmax, dxdt_b, '--d')
+plot(tmin:tstep:tmax, dxdt_c, '--d')
+plot(tmin:tstep:tmax, dxdt_d, '--d')
+plot(tmin:tstep:tmax, dxdt_e, '--d')
+
+% Improved euler method:
+
+dxdt_a2 = eul2(ode2, tmin, tmax, tstep, x0);
+dxdt_b2 = eul2(ode2, tmin, tmax, tstep, x0b);
+dxdt_c2 = eul2(ode2, tmin, tmax, tstep, x0c);
+dxdt_d2 = eul2(ode2, tmin, tmax, tstep, x0d);
+dxdt_e2 = eul2(ode2, tmin, tmax, tstep, x0e);
+
+plot(tmin:tstep:tmax, dxdt_a2, ':*')
+plot(tmin:tstep:tmax, dxdt_b2, ':*')
+plot(tmin:tstep:tmax, dxdt_c2, ':*')
+plot(tmin:tstep:tmax, dxdt_d2, ':*')
+plot(tmin:tstep:tmax, dxdt_e2, ':*')
 
 hold off
 
@@ -133,12 +174,47 @@ x0 = 0;
 x0b = -2;
 x0c = 1;
 [t,x] = ode23(@(t,x) 1+0.5*cos(x), tspan, [x0 x0b x0c]);
-plot(t, x, '-o')
+plot(t, x, '-d')
+hold on
 title('xdot = 1 + 0.5*cos(x)')
 xlabel('t')
 ylabel('x')
 
 title(legend(num2str(x0), num2str(x0b), num2str(x0c)), 'x0')
+
+% Euler method:
+
+ode3 = @(x) 1+0.5*cos(x);
+tmin = 0;
+tmax = 5;
+tstep = 0.1;
+dxdt_a = eul(ode3, tmin, tmax, tstep, x0);
+dxdt_b = eul(ode3, tmin, tmax, tstep, x0b);
+dxdt_c = eul(ode3, tmin, tmax, tstep, x0c);
+% dxdt_d = eul(ode2, tmin, tmax, tstep, x0d);
+% dxdt_e = eul(ode2, tmin, tmax, tstep, x0e);
+
+plot(tmin:tstep:tmax, dxdt_a, '--d')
+plot(tmin:tstep:tmax, dxdt_b, '--d')
+plot(tmin:tstep:tmax, dxdt_c, '--d')
+% plot(tmin:tstep:tmax, dxdt_d, '--')
+% plot(tmin:tstep:tmax, dxdt_e, '--')
+
+% Improved euler method:
+
+dxdt_a2 = eul2(ode3, tmin, tmax, tstep, x0);
+dxdt_b2 = eul2(ode3, tmin, tmax, tstep, x0b);
+dxdt_c2 = eul2(ode3, tmin, tmax, tstep, x0c);
+% dxdt_d2 = eul2(ode2, tmin, tmax, tstep, x0d);
+% dxdt_e2 = eul2(ode2, tmin, tmax, tstep, x0e);
+
+plot(tmin:tstep:tmax, dxdt_a2, ':*')
+plot(tmin:tstep:tmax, dxdt_b2, ':*')
+plot(tmin:tstep:tmax, dxdt_c2, ':*')
+% plot(tmin:tstep:tmax, dxdt_d2, ':')
+% plot(tmin:tstep:tmax, dxdt_e2, ':')
+
+hold off
 
 % 4) Globally unstable. Increases for all x0.
 
@@ -153,8 +229,7 @@ title(legend(num2str(x0), num2str(x0b), num2str(x0c)), 'x0')
 %
 % 3)
 
-tspan = [0 2];
-
+tspan = [0 .25];
 x0 = 0;
 x0b = -1;
 x0c = 1;
@@ -166,8 +241,8 @@ title('xdot = 1 - x^ 14')
 xlabel('t')
 ylabel('x')
 
-% hold on
-% 
+hold on
+
 % x0b = -1;
 % [t,x] = ode23(@(t,x) 1 - x^14, tspan, x0b);
 % plot(t, x, ':d')
@@ -183,6 +258,40 @@ ylabel('x')
 % x0e = -1.5;
 % [t,x] = ode23(@(t,x) 1 - x^14, tspan, x0e);
 % plot(t, x, '-*')
+
+% Euler method:
+
+ode4 = @(x) 1 - x.^14;
+tmin = 0;
+tmax = 0.25;
+tstep = 0.006;
+dxdt_a = eul(ode4, tmin, tmax, tstep, x0);
+dxdt_b = eul(ode4, tmin, tmax, tstep, x0b);
+dxdt_c = eul(ode4, tmin, tmax, tstep, x0c);
+dxdt_d = eul(ode4, tmin, tmax, tstep, x0d);
+% dxdt_e = eul(ode2, tmin, tmax, tstep, x0e);
+
+plot(tmin:tstep:tmax, dxdt_a, '--d')
+plot(tmin:tstep:tmax, dxdt_b, '--d')
+plot(tmin:tstep:tmax, dxdt_c, '--d')
+plot(tmin:tstep:tmax, dxdt_d, '--d')
+% plot(tmin:tstep:tmax, dxdt_e, '--')
+
+% Improved euler method:
+
+dxdt_a2 = eul2(ode4, tmin, tmax, tstep, x0);
+dxdt_b2 = eul2(ode4, tmin, tmax, tstep, x0b);
+dxdt_c2 = eul2(ode4, tmin, tmax, tstep, x0c);
+dxdt_d2 = eul2(ode4, tmin, tmax, tstep, x0d);
+% dxdt_e2 = eul2(ode2, tmin, tmax, tstep, x0e);
+
+plot(tmin:tstep:tmax, dxdt_a2, ':*')
+plot(tmin:tstep:tmax, dxdt_b2, ':*')
+plot(tmin:tstep:tmax, dxdt_c2, ':*')
+plot(tmin:tstep:tmax, dxdt_d2, ':*')
+% plot(tmin:tstep:tmax, dxdt_e2, ':')
+
+hold off
 
 title(legend(num2str(x0), num2str(x0b), num2str(x0c), num2str(x0d)), 'x0')
 
