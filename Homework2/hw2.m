4628eb64bd6a20bfce12d4bd9f5d6a6b5aa48ccb
R Steiner <r.steiner@uconn.edu>
2016-09-19 11:29:32 -0400

    Add sims and comments

--- Homework2/hw2.m
+++ Homework2/hw2.m
@@ -53,6 +53,48 @@ plot(r,zeros(1,length(r)), 'k');
 plot(zeros(1,length(r)), r, 'k');
 hold off
 
+%%% Simulate
+
+tspan = [0 5];
+x0 = 0;
+x0b = -1;
+yr1 = @(t, x) (x.*r1) - x.*(1-x);
+[t,x] = ode23(yr1, tspan, [x0 x0b]);
+figure(3)
+subplot(2,2,1)
+plot(t, x, '-d')
+title('xdot = -x(1-x)')
+xlabel('t')
+ylabel('x')
+title(legend(num2str(x0), num2str(x0b)), 'x0')
+
+yr2 = @(t, x) (x.*r2) - x.*(1-x);
+[t,x] = ode23(yr2, tspan, [x0 x0b]);
+figure(3)
+subplot(2,2,2)
+plot(t, x, '-d')
+title('xdot = x - x(1-x)')
+xlabel('t')
+ylabel('x')
+title(legend(num2str(x0), num2str(x0b)), 'x0')
+
+yr3 = @(t, x) (x.*r3) - x.*(1-x);
+[t,x] = ode23(yr3, tspan, [x0 -0.8]);
+figure(3)
+subplot(2,2,3)
+plot(t, x, '-d')
+title('xdot = 2x - x(1-x)')
+xlabel('t')
+ylabel('x')
+title(legend(num2str(x0), num2str(-0.8)), 'x0')
+
+%%
+% For all values of r, x = 0 is a fixed point. For r < 1 and x0 = -1, the
+% system converges on zero quickly. For r = 1 and x0 = -1, the system
+% converges on zero, but takes more time to do so. For r > 1, both x = 0
+% and x = -1 are fixed points, and when x0 = -0.8, the system converges on x
+% = -1 quickly.
+
 %% xdot = x - rx(1-x)
 %
 % A transcritical bifurcation occurs at r = 1
@@ -100,6 +142,46 @@ plot(r,zeros(1,length(r)), 'k');
 plot(zeros(1,length(r)), r, 'k');
 hold off
 
+%%% Simulate
+
+tspan = [0 5];
+x0 = 0;
+x0b = -1;
+yr1 = @(t, x) x - r1.*x.*(1-x);
+[t,x] = ode23(yr1, tspan, [x0 -0.8]);
+figure(3)
+subplot(2,2,1)
+plot(t, x, '-d')
+title('xdot = x - 0.5x(1-x)')
+xlabel('t')
+ylabel('x')
+title(legend(num2str(x0), num2str(-0.8)), 'x0')
+
+yr2 = @(t, x) x - r2.*x.*(1-x);
+[t,x] = ode23(yr2, tspan, [x0 x0b]);
+figure(3)
+subplot(2,2,2)
+plot(t, x, '-d')
+title('xdot = x - x(1-x)')
+xlabel('t')
+ylabel('x')
+title(legend(num2str(x0), num2str(x0b)), 'x0')
+
+yr3 = @(t, x) x - r3.*x.*(1-x);
+[t,x] = ode23(yr3, tspan, [x0 x0b]);
+figure(3)
+subplot(2,2,3)
+plot(t, x, '-d')
+title('xdot = x - 2x(1-x)')
+xlabel('t')
+ylabel('x')
+title(legend(num2str(x0), num2str(x0b)), 'x0')
+
+%%
+% For all values of r, x = 0 is a fixed point. For r < 1, x = -1 is also a fixed point.
+% When x0 = -0.8, system converges on x = -1. For r = 1 and x0 = -1, the system
+% converges on zero. For r > 1 and x0 = -1, the system converges on zero quickly.
+
 %% xdot = x - rx^3
 %
 % A supercritical pitchfork bifurcation occurs at r = 1.
@@ -146,10 +228,6 @@ ylabel('xdot');
 axis([-3 3 -1 4]);
 hold off
 
-% title(legend(num2str(r1), num2str(r2), num2str(r3)), 'r');
-
-% hold off
-
 %%% Bifurcation diagram
 
 r = -4:0.01:4;
@@ -165,8 +243,44 @@ plot(r,zeros(1,length(r)), 'k');
 plot(zeros(1,length(r)), r, 'k');
 hold off
 
+%%% Simulate
 
+tspan = [0 3];
+x0 = 0;
+x0b = -1;
+yr1 = @(t, x) x - (x.^3).*r1;
+[t,x] = ode23(yr1, tspan, [x0 x0b]);
+figure(3)
+subplot(2,2,1)
+plot(t, x, '-d')
+title('xdot = x - 0.5x^3')
+xlabel('t')
+ylabel('x')
+title(legend(num2str(x0), num2str(x0b)), 'x0')
 
+yr2 = @(t, x) x - (x.^3).*r2;
+[t,x] = ode23(yr2, tspan, [x0 -0.8]);
+figure(3)
+subplot(2,2,2)
+plot(t, x, '-d')
+title('xdot = x - x^3')
+xlabel('t')
+ylabel('x')
+title(legend(num2str(x0), num2str(x0b)), 'x0')
 
+yr3 = @(t, x) x - (x.^3).*r3;
+[t,x] = ode23(yr3, tspan, [x0 x0b]);
+figure(3)
+subplot(2,2,3)
+plot(t, x, '-d')
+title('xdot = x - 2x^3')
+xlabel('t')
+ylabel('x')
+title(legend(num2str(x0), num2str(x0b)), 'x0')
 
+%%
+% For all values of r, x = 0 is a fixed point. For r = 0.5 and x0 = -1, the
+% system converges on -sqrt(0.5)/0.5. For r = 1, x = -1 is a fixed point,
+% and when x0 = -0.8, the system converges on x = -1. For r = 2 and x0 =
+% -1, the system onverges on -sqrt(2)/2.
 
