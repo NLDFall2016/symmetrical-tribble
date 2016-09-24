cf654615a184941290375195b8a536df187ee908
Rachael Steiner <rachael.steiner@uconn.edu>
2016-09-24 15:34:08 -0400

    Move individual modules to separate directory

---
+++ modules/eul2.m
@@ -0,0 +1,15 @@
+function eulx = eul2(dxdt, t0, tn, dt, x0) 
+    %%% Adapted from in-class example and eul.m
+    t = t0:dt:tn;
+    nsteps = length(t);
+    eulx = zeros(1, nsteps);
+    eulx(1) = x0;
+    
+    for n = 2:nsteps
+        last = eulx(n-1);
+        dx = feval(dxdt, last);
+        xish = last + dx*dt;
+        dxish = feval(dxdt, xish);
+        eulx(n) = last + ((dx + dxish)/2)*dt;
+    end
+end
